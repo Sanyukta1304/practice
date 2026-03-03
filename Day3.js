ed8d54c9031fdebaa4bbde7f943ec1f150868b8e
Sanyukta1304 <dassanyukta9@gmail.com>
2026-03-03 10:15:55 +0530

    add additional implementations of the add function and demonstrate set operations

--- Day3.js
+++ Day3.js
@@ -28,5 +28,27 @@ function add(...args){
 add(5,10,89,90,100) //extra arguments are ignored by the function, only the first two arguments are used.   ;
 
 
+function add(a,b){
+    return a+b;
+}
+console.log(add(5,10));
 
 
+var add=(a,b)=>a+b; //arrow function
+console.log(add(5,10));
+
+var add=(a,b)=>{
+    console.log(a,b); //arrow function with block body
+    return a+b;
+}           
+console.log(add(5,10));
+
+///function wtre ananonymious function-assinment 
+
+
+//set 
+var s = new Set([1,2,3,3,4,4,4,4,5]);   //set is a collection of unique values
+var s2 = new Set([4,5,6,7,8]); //empty set
+console.log(s,s2); //set will remove duplicate values
+//set operations
+console.log(s.union(s2));

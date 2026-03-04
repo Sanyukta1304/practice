2d41708dc353f841d516b2522ab8f572c44033c9
Sanyukta1304 <dassanyukta9@gmail.com>
2026-03-04 10:12:13 +0530

    add switch case statement to handle days of the week

--- day4.js
+++ day4.js
@@ -87,5 +87,33 @@ if (age==18){
 }   
 
 
+//switch case statement
+var day="wednesday";
+switch(day){
+    case 'monday':
+        console.log('today is monday'); 
+        break;
+    case 'tuesday':
+        console.log('today is tuesday');    
+        break;
+    case 'wednesday':
+        console.log('today is wednesday');    
+        break;
+    case 'thursday':
+        console.log('today is thursday');    
+        break;
+    case 'friday':
+        console.log('today is friday');    
+        break;
+    case 'saturday':
+        console.log('today is saturday');    
+        break;
+    case 'sunday':
+        console.log('today is sunday');    
+        break;
+    default:
+        console.log('invalid day');    
+}       
+
 
          

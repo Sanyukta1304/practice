be47d3b47dfe6e7d8b70593f92e11411143f5843
Sanyukta1304 <dassanyukta9@gmail.com>
2026-03-07 19:51:20 +0530

    add initial comments to explain JavaScript syntax and usage

---
+++ day6.js
@@ -0,0 +1,12 @@
+// This is a simple comment in JavaScript
+// You can write comments to explain your code or to leave notes for yourself or other developers
+// Comments are ignored by the JavaScript engine and do not affect the execution of the code
+// Here is an example of a single-line comment
+/* This is a multi-line comment in JavaScript
+ You can use multi-line comments to write longer explanations or to comment out blocks of code
+ Multi-line comments start with /* and end with *\/ (without the backslash)
+*/  
+// You can also use comments to temporarily disable code without deleting it
+// For example, if you want to test a piece of code without running a certain line, you can comment it out like this:
+// console.log("This line is commented out and will not run");      
+// Remember to use comments wisely and keep them clear and concise to improve the readability of your code  
